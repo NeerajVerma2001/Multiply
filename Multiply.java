2bd87d42e8636b0d181a98a83144611df7d8590e
Neeraj Verma <113288731+NeerajVerma2001@users.noreply.github.com>
2023-06-01 22:53:45 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,13 @@
+
+public class Multiply
+{
+ public static void mul(int a,int b)
+ {
+	 System.out.println(a*b);
+ }
+ 
+ public static void main(String[] args) {
+	int a=10,b=2;
+	mul(a, b);
+}
+}
